7620057db8a3933b8c8d4f28c804ff4a44099538
shahinvr3280 <arashtrader.alpari@gmail.com>
2025-04-18 12:24:34 +0330

    Update ExposureEngine.cs

--- ExposureEngine.cs
+++ ExposureEngine.cs
@@ -1,1 +1,17 @@
+using UnityEngine;
 
+public class ExposureEngine : MonoBehaviour
+{
+    [Range(0,1)] public float virtualAlpha = 0f;
+    public Material blendMaterial;
+
+    void Update()
+    {
+        blendMaterial.SetFloat("_VirtualAlpha", virtualAlpha);
+    }
+
+    public void SetIntensity(float t)
+    {
+        virtualAlpha = Mathf.Clamp01(t);
+    }
+}
